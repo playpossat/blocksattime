bd61082d3f3330b75b429c294f85a81d414cd5ea
playpossat <satoshimadness@outlook.com>
2015-08-17 00:42:20 +0200

    Update main.h

--- src/main.h
+++ src/main.h
@@ -37,7 +37,7 @@ static const unsigned int MAX_INV_SZ = 50000;
 static const int64_t MIN_TX_FEE = 10000;
 static const int64_t MIN_RELAY_TX_FEE = MIN_TX_FEE;
 static const int64_t MAX_MONEY = 80000000 * COIN;
-static const int64_t MAX_MINT_PROOF_OF_STAKE = 0.02 * COIN;	// 5% Interest 
+static const int64_t MAX_MINT_PROOF_OF_STAKE = 0.02 * COIN;	// 2% Interest 
 static const int64_t COIN_YEAR_REWARD = 2 * CENT; //(output to console will be updated)
 
 #define FOUNDATION "0x"
